ba1c737d70789edec165044648ad06df7dee5bcc
risou <ayamachi@outlook.com>
2020-04-28 23:34:19 +1200

    Completed Basic TitleNav component

--- src/DisplayComponents/TitleNav.Component.js
+++ src/DisplayComponents/TitleNav.Component.js
@@ -5,9 +5,18 @@ import './TitleNav.css';
 export default class TitleNav extends Component  {
   render(){
     return (
-      <div className="TitleNav">        
-        <div> ULTRA WEALTH ({this.props.page})  </div>
-        <div> NAV MENU </div>
+      <div className="TitleNav">   
+        <div className="wrapper"> 
+            <div className="col Padding"> 
+                <div></div>
+            </div>
+            <div className="col Title"> 
+                <div> U L T R A  W E A L T H ({this.props.page})  </div>
+            </div>
+            <div className="col MenuBtn"> 
+                <div> ☰ </div>
+            </div>
+        </div>     
       </div>
     );
   }
