b244065d3dbcb850e2c30ffbab807e68a55aed28
risou <ayamachi@outlook.com>
2020-04-29 00:10:32 +1200

    Completed rough prototype of field grid and cells

---
+++ src/GameComponents/Cell.Component.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+import './Cell.css';
+
+export default class Cell extends Component  {
+  render(){
+    return (
+      <div className="example"> 
+        WOOD 
+      </div>
+    );
+  }
+}
--- src/GameComponents/MineGrid.Component.js
+++ src/GameComponents/MineGrid.Component.js
@@ -1,10 +1,26 @@
 import React, {Component} from 'react';
 
+import './MineGrid.css';
+
+import Cell from './Cell.Component';
+
 export default class MineGrid extends Component  {
   render(){
     return (
       <div className="MineGrid">
-        MineGrid Works
+        <div className="MineGrid-wrapper field">
+         <Cell></Cell>
+         <Cell></Cell>
+         <Cell></Cell>
+         <Cell></Cell>
+         <Cell></Cell>
+         <Cell></Cell>
+         <Cell></Cell>
+         <Cell></Cell>
+         <Cell></Cell>
+         <Cell></Cell>
+         <Cell></Cell>
+        </div>
       </div>
     );
   }
