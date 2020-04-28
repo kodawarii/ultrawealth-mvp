a366db78dd410277b7f1f9fac8637e4a3dbd8769
risou <ayamachi@outlook.com>
2020-04-28 23:57:01 +1200

    Added minor tweaks to styles of titleNav and dataDisplay components

--- src/GameComponents/DataDisplay.Component.js
+++ src/GameComponents/DataDisplay.Component.js
@@ -3,11 +3,16 @@ import React, {Component} from 'react';
 import './DataDisplay.css';
 
 export default class DataDisplay extends Component  {
+
+  numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
   render(){
     return (
       <div className="DataDisplay">
-        <div> Cash: $10,000 </div>
-        <div> <button className="btn-inventory"> Inventory </button> </div>
+        <div className="cash"> Cash: ${this.numberWithCommas(this.props.cash)} </div>
+        <div className="inventory"> <button className="btn-inventory"> Inventory </button> </div>
       </div>
     );
   }
--- src/ScreenComponents/Main.Component.js
+++ src/ScreenComponents/Main.Component.js
@@ -5,11 +5,20 @@ import DataDisplay from '../GameComponents/DataDisplay.Component';
 import MineGrid from '../GameComponents/MineGrid.Component';
 
 export default class Main extends Component  {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      cash: 10000
+    }
+  }
+
   render(){
     return (
       <div className="Main">
         <TitleNav page={"Main"}/>
-        <DataDisplay />
+        <DataDisplay cash={this.state.cash}/>
         <MineGrid />
       </div>
     );
