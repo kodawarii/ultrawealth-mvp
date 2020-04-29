516a536d9a121c4d83c27fc596decbbb8fbbcda7
risou <ayamachi@outlook.com>
2020-04-30 00:04:06 +1200

    Completed basic layout and design for DataDisplay component

--- src/GameComponents/DataDisplay.Component.js
+++ src/GameComponents/DataDisplay.Component.js
@@ -11,8 +11,23 @@ export default class DataDisplay extends Component  {
   render(){
     return (
       <div className="DataDisplay">
-        <div className="cash"> Cash: ${this.numberWithCommas(this.props.cash)} </div>
-        <div className="inventory"> <button className="btn-inventory"> Inventory </button> </div>
+        <div className="DataDisplay-1Data">
+          <ul className="DataDisplay-1Data-ul">
+            <li className="cash">
+                <div className="DataDisplay-Header">Cash</div>
+                <div className="DataDisplay-Figure">${this.numberWithCommas(this.props.cash)}</div> 
+                <div className="DataDisplay-Figure2"> +$500 (+5.04%) </div>
+            </li>
+          </ul>
+        </div>        
+        <div className="DataDisplay-2Buttons">
+          <ul className="DataDisplay-2Buttons-ul">
+            <li><button className="DataDisplay-btn"> Inventory </button></li>
+            <li><button className="DataDisplay-btn"> Equips </button></li>
+            <li><button className="DataDisplay-btn"> Assets </button></li>
+            <li><button className="DataDisplay-btn"> SHOP </button></li>
+          </ul>
+        </div>
       </div>
     );
   }
